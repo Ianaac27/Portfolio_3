b91665212f73dc15ebe313d2ee6c5a86d66ac1a0
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-03-04 15:10:34 -0800

    developed skills component for main body with css, installed fontawesome for icons

--- src/components/body_container/main_body/MainBody.js
+++ src/components/body_container/main_body/MainBody.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Introduction from "./Introduction";
 import AboutMe from "./AboutMe";
-// import "../styles/Body.css";
+import MySkills from "./MySkills";
 
 function MainBody() {
     return (
         <main>
             <Introduction />
             <AboutMe />
+            <MySkills />
         </main>
     )
 }
---
+++ src/components/body_container/main_body/MySkills.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "../../../styles/MySkills.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faJsSquare } from '@fortawesome/free-brands-svg-icons'
+import { faCss3Alt } from '@fortawesome/free-brands-svg-icons'
+import { faHtml5 } from '@fortawesome/free-brands-svg-icons'
+import { faBootstrap } from '@fortawesome/free-brands-svg-icons'
+import { faNode } from '@fortawesome/free-brands-svg-icons'
+import { faNpm } from '@fortawesome/free-brands-svg-icons'
+import { faReact } from '@fortawesome/free-brands-svg-icons'
+import { faDatabase } from '@fortawesome/free-solid-svg-icons'
+
+
+
+function MySkills() {
+    return (
+        <section className="content container" id="skill">
+            <h2 className="section-title" id="skill-title"><strong>My skills</strong></h2>
+            <div className="skills">
+                <div className="skill-row" id="skill-row">
+                    <i className="skill-icon">
+                        <FontAwesomeIcon icon={faJsSquare}/>
+                    </i>
+                    <i className="skill-icon">
+                        <FontAwesomeIcon icon={faCss3Alt}/>
+                    </i>
+                    <i className="skill-icon">
+                        <FontAwesomeIcon icon={faHtml5}/>
+                    </i>
+                    <i className="skill-icon">
+                        <FontAwesomeIcon icon={faBootstrap}/>
+                    </i>
+                    <i className="skill-icon">
+                        <FontAwesomeIcon icon={faNode}/>
+                    </i>
+                    <i className="skill-icon">
+                        <FontAwesomeIcon icon={faNpm}/>
+                    </i>
+                    <i className="skill-icon">
+                        <FontAwesomeIcon icon={faReact}/>
+                    </i>
+                    <i className="skill-icon">
+                        <FontAwesomeIcon icon={faDatabase}/>
+                    </i>     
+                 </div>
+            </div> 
+        </section>
+    )
+}
+
+export default MySkills;
