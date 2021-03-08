0b210eb93920af001e33fc2cabd08dba147be682
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-03-08 11:40:13 -0800

    established the project page routes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from "./components/body_container/home/Home";
+
+//Project pages
 import Project1 from "./components/body_container/project_pages/ProjectOne";
-import Project2 from "./components/body_container/project_pages/ProjectTwo"
+import Project2 from "./components/body_container/project_pages/ProjectTwo";
+import Project3 from "./components/body_container/project_pages/ProjectThree";
+import Project4 from "./components/body_container/project_pages/ProjectFour";
+import Project5 from "./components/body_container/project_pages/ProjectFive";
+import Project6 from "./components/body_container/project_pages/ProjectSix";
+import Project7 from "./components/body_container/project_pages/ProjectSeven";
+import Project8 from "./components/body_container/project_pages/ProjectEight";
+import Project9 from "./components/body_container/project_pages/ProjectNine";
+import Project10 from "./components/body_container/project_pages/ProjectTen";
 
 
 class App extends React.Component {
@@ -26,12 +36,16 @@ render() {
             <Route exact path="/">
               <Home />
             </Route>
-            <Route path="/project1">
-              <Project1 />
-            </Route>
-            <Route path="/project2">
-              <Project2 />
-            </Route>
+            <Route path="/project1"><Project1 /></Route>
+            <Route path="/project2"><Project2 /></Route>
+            <Route path="/project3"><Project3 /></Route>
+            <Route path="/project4"><Project4 /></Route>
+            <Route path="/project5"><Project5 /></Route>
+            <Route path="/project6"><Project6 /></Route>
+            <Route path="/project7"><Project7 /></Route>
+            <Route path="/project8"><Project8 /></Route>
+            <Route path="/project9"><Project9 /></Route>
+            <Route path="/project10"><Project10 /></Route>
           </Switch>
         <Footer />
       </Router>
--- src/components/body_container/project_pages/ProjectNine.js
+++ src/components/body_container/project_pages/ProjectNine.js
@@ -3,31 +3,28 @@ import "../../../styles/ProjectPage.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
-import neighborhood_img from "../../../images/project_images/neighborhood2.jpg";
+import burger_dev_img from "../../../images/project_images/burgerd5.jpg";
 
-function Project10() {
+function Project9() {
     return (
         <main className="project-main">
               <section className="project-left">
-                    <h2 className="section-title" id="project-title">The Neighborhood</h2>
-                    <p className="para-subtitle-accent" id="project-sub">Get connected</p>
+                    <h2 className="section-title" id="project-title">Burger Devourer</h2>
+                    <p className="para-subtitle-accent" id="project-sub">Eyes on the fries</p>
                     <div className="section-body">
-                        <p className="para-project">The Neighborhood is a virtual neighborhood that allows users to connect with others based on common interest. With COVID running the world, finding connection, friendship, and socializing outside of the home has become more complicated than ever. Users have the freedom to create a variety of interests for others to get connected, and who may share the same interest. At this point, the app only offers user connection via email, however, there is much room for future development and improvement to grow the app in other means of communication.</p>
-                        <p className="para-project">This app was created to demonstrate our development teams’ ability to gather user input and store that information within a MySQL database. From here we can insert, display, and update user information and category information to make the app function as it does.</p>
-                        <p className="para-project">My role in this project was mainly on the backend as I was responsible for setting up the database and utilizing Sequelize to connect the database, as well as establish API routes for specific queries. As mentioned, our app has the functionality to add new users and categories, display the user and category information, as well as update the users Category ID so they can change their interests. The relationship between the tables is currently a One-to-Many but has room to improve to Many-to-Many so users can have the ability to choose many interests at a time, rather than one.</p>
-                        <p className="para-project">I was also responsible for majority of the functionality within the website utilizing jQuery, ajax, and Handlebars.js, as well as the websites responsiveness for mobile friendliness.</p>
+                        <p className="para-project">The Burger Devourer is a fun application that displays my knowledge in MVC and how to provide the user with the ability to view the content, create something new within the content, update, and delete. This is done through the Node.js server while using the npm packages Express, Handlebars, and MySQL. With Node and Express, I am able to connect MySQL workbench so I could have a running database. With this, I am able to configure this data through my ORM and model files of my application. This information would then be passed through the controller and onto the views file which contains the content to display the application and to provide the functions listed above for the user. The main webpage is generated through Handlebars. Once the user inputs their information, it will then pass through to the controller, and back down to the MySQL database for any database modifications. I have deployed this application through Heroku.</p>
                     </div> 
               </section>
               
               <section className="project-right">
                   <div className="project-item">
-                      <a href="https://neighborhood-connectwithothers.herokuapp.com/"><img className="portfolio-img" id="project-img" src={neighborhood_img} alt=""/></a>
+                      <a href="https://burger-devourer-mvc.herokuapp.com/"><img className="portfolio-img" id="project-img" src={burger_dev_img} alt=""/></a>
                   </div>
                   <div className="project-links">
-                      <a className="project-url" href="https://neighborhood-connectwithothers.herokuapp.com/">Live Website
+                      <a className="project-url" href="https://burger-devourer-mvc.herokuapp.com/">Live Website
                             <FontAwesomeIcon className="url-icon" icon={faLink}></FontAwesomeIcon>
                       </a>
-                      <a className="project-url" href="https://github.com/zhanez/Neighborhood">Git Hub Repository
+                      <a className="project-url" href="https://github.com/Ianaac27/Burger_Devourer">Git Hub Repository
                             <FontAwesomeIcon className="url-icon" icon={faGithub}></FontAwesomeIcon>
                       </a>
                   </div>  
@@ -36,4 +33,4 @@ function Project10() {
     )
 }
 
-export default Project10;
+export default Project9;
