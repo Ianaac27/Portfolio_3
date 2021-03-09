ee5654cd8d967fe7b79804ab3c5b52bb364a6376
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-03-08 18:20:13 -0800

    updated employee directory subtitle

--- src/components/body_container/project_pages/ProjectThirteen.js
+++ src/components/body_container/project_pages/ProjectThirteen.js
@@ -10,7 +10,7 @@ function Project13() {
         <main className="project-main">
               <section className="project-left">
                     <h2 className="section-title" id="project-title">Employee Directory</h2>
-                    <p className="para-subtitle-accent" id="project-sub">Check the directory</p>
+                    <p className="para-subtitle-accent" id="project-sub">Sort and filter</p>
                     <div className="section-body">
                         <p className="para-project">The Employee Directory is an app in which organizes and protects employee information by filtering searches based on the employee’s first and last name. This app is populated with the Random User npm package, and uses React.js to establish the rendering components and functionality for each component.</p>
                         <p className="para-project">A couple of the functions developed within this app is the search function in which filters each employee’s first and last name, as well as a sort function which sorts the directory of employees by last name. I also utilized Bootstrap as the foundation for the styling and responsiveness for this app.</p>
